f955fcb88383a5a81f80b00a66d6190cbddf348b
anersisian <46788253+anersisian@users.noreply.github.com>
2019-09-24 21:46:02 -0400

    Nav/Home/Login/Register

--- Project/src/app/app-routing.module.ts
+++ Project/src/app/app-routing.module.ts
@@ -2,8 +2,17 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { MainPageComponent } from "./main-page/main-page.component";
+import { LoginComponent } from "./login/login.component";
+import { NotFoundPageComponent } from "./not-found-page/not-found-page.component";
+import { RegisterComponent } from "./register/register.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: MainPageComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "login", component: LoginComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "**", component: NotFoundPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -7,14 +7,32 @@ import { AppComponent } from "./app.component";
 import { MainPageComponent } from "./main-page/main-page.component";
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { LoginComponent } from "./login/login.component";
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+import { NotFoundPageComponent } from "./not-found-page/not-found-page.component";
+import { RegisterComponent } from './register/register.component';
+
+const appRoutes: Routes = [{ path: "login", component: LoginComponent }];
+
 @NgModule({
-  declarations: [AppComponent, MainPageComponent, NavBarComponent],
+  declarations: [
+    AppComponent,
+    MainPageComponent,
+    NavBarComponent,
+    LoginComponent,
+    NotFoundPageComponent,
+    RegisterComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     AngularSvgIconModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    RouterModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
