518c7abce928e80191f46bcf1189902121056c69
akessa <nersisian-aik@outlook.com>
2020-03-24 03:00:47 -0400

    dark partially implemented

--- Project/src/app/main-page/main-page.component.ts
+++ Project/src/app/main-page/main-page.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { DataModelManagerService } from '../data-model-manager.service';
 
 @Component({
-  selector: 'app-main-page',
-  templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+	selector: 'app-main-page',
+	templateUrl: './main-page.component.html',
+	styleUrls: [ './main-page.component.css' ]
 })
 export class MainPageComponent implements OnInit {
+	whichMode: Boolean = true;
 
-  constructor() { }
+	constructor(private m: DataModelManagerService) {}
 
-  ngOnInit() {
-  }
-
+	ngOnInit() {
+		this.m.theme.subscribe((mode) => (this.whichMode = mode));
+	}
 }
